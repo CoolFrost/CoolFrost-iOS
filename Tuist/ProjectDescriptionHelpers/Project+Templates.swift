cb852c3ba3d65b5437176b11402ac9b03f63f95d
Will <dltmdals0608@naver.com>
2024-08-29 22:59:18 +0900

    Update: Tuist 4 버전 설정 추가

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -16,44 +16,43 @@ extension Project {
                        organizationName: "tuist.io",
                        targets: targets)
     }
-
+    
     // MARK: - Private
-
+    
     /// Helper function to create a framework target and an associated unit test target
     private static func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
         let sources = Target.target(name: name,
-                                    
-                platform: platform,
-                product: .framework,
-                bundleId: "io.tuist.\(name)",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Sources/**"],
-                resources: [],
-                dependencies: [])
+                                    destinations: .iOS,
+                                    product: .framework,
+                                    bundleId: "io.tuist.\(name)",
+                                    infoPlist: .default,
+                                    sources: ["Targets/\(name)/Sources/**"],
+                                    resources: [],
+                                    dependencies: [])
         let tests = Target.target(name: "\(name)Tests",
-                platform: platform,
-                product: .unitTests,
-                bundleId: "io.tuist.\(name)Tests",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Tests/**"],
-                resources: [],
-                dependencies: [.target(name: name)])
+                                  destinations: .iOS,
+                                  product: .unitTests,
+                                  bundleId: "io.tuist.\(name)Tests",
+                                  infoPlist: .default,
+                                  sources: ["Targets/\(name)/Tests/**"],
+                                  resources: [],
+                                  dependencies: [.target(name: name)])
         return [sources, tests]
     }
-
+    
     /// Helper function to create the application target and the unit test target.
     private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
-        let platform: Platform = platform
+//        let platform: Platform = platform
         let infoPlist: [String: Plist.Value] = [
             "CFBundleShortVersionString": "1.0",
             "CFBundleVersion": "1",
-"UIMainStoryboardFile": "",
+            "UIMainStoryboardFile": "",
             "UILaunchStoryboardName": "LaunchScreen"
-            ]
-
+        ]
+        
         let mainTarget = Target.target(
             name: name,
-            platform: platform,
+            destinations: .iOS,
             product: .app,
             bundleId: "io.tuist.\(name)",
             infoPlist: .extendingDefault(with: infoPlist),
@@ -61,17 +60,17 @@ extension Project {
             resources: ["Targets/\(name)/Resources/**"],
             dependencies: dependencies
         )
-
-        let testTarget = Target(
+        
+        let testTarget = Target.target(
             name: "\(name)Tests",
-            platform: platform,
+            destinations: .iOS,
             product: .unitTests,
             bundleId: "io.tuist.\(name)Tests",
             infoPlist: .default,
             sources: ["Targets/\(name)/Tests/**"],
             dependencies: [
                 .target(name: "\(name)")
-        ])
+            ])
         return [mainTarget, testTarget]
     }
 }
